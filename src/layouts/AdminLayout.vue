47c2a1e380d6c645f8d42da1bb07fec208069b98
aitorcajas <acajas@stemdo.io>
2025-01-22 09:45:01 +0100

    Refactorización de archivos

--- src/layouts/AdminLayout.vue
+++ src/layouts/AdminLayout.vue
@@ -17,68 +17,6 @@
     </div>
 </template>
 
-<table border="1">
-<caption>
-    Rental price
-</caption>
-<thead>
-    <tr>
-        <td></td>
-        <th id="small" scope="col">
-            Small car
-        </th>
-        <th id="big" scope="col">
-            Big Car
-        </th>
-    </tr>
-</thead>
-<tbody>
-    <tr>
-        <th id="paris" class="span" colspan="3" scope="colgroup">
-            Paris
-        </th>
-        </tr>
-        <tr>
-        <th headers="paris" id="day1">
-            1 day
-        </th>
-        <td headers="paris day1 big"> <!-- Noncompliant, referencing the column "big" instead of "small" -->
-            11 euros
-        </td>
-        <td headers="berlin day1 big"> <!-- Noncompliant, there is no header with id "berlin" -->
-            50 euros
-        </td>
-        </tr>
-    </tr>
-</tbody>
-</table>
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-
-Table cells should reference their headers
-Table cells should reference their headers
-Table cells should reference their headers
-
 <script>
 import AdminMenu from "@/components/Menus/AdminMenu.vue";
 import AccountMenu from "../components/Menus/AccountMenu.vue";
